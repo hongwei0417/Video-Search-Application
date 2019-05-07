a792fdceef5463dc30cacb4212ffe3db46c56873
Hongwei <iamhongwei0417@gmail.com>
2019-05-08 00:05:03 +0800

    影片列表製作...

--- View/Video.py
+++ View/Video.py
@@ -1,10 +1,16 @@
 import tkinter as tk
+import requests
 from PIL import ImageTk, Image
+from VideoObj import Video
+from io import BytesIO
 
 wh = 500
-ww = 1200
+ww = 800
+fh = 200
+fw = 760
 bgc = "#121212"
 top_bgc = "#272727"
+fbgc = "#1C1C1C"
 
 window = tk.Tk()
 window.title('Video Search')
@@ -36,11 +42,62 @@ canvas.create_window(0,0, window=frame, anchor="nw")
 frame.bind("<Configure>", lambda event, canvas=canvas: onFrameConfigure(canvas))
 
 
-top_frame = tk.Frame(frame, bg=top_bgc, width=ww, height=50)
+top_frame = tk.Frame(frame, bg=top_bgc, width=ww, height=60)
 top_frame.place(x=0, y=0)
 
-ytFrame = tk.Frame(frame, bg="white", width=600, height=50)
-ytFrame.place(x=100, y=100)
+ytFrame = tk.Frame(frame, bg=fbgc, width=fw, height=fh)
+ytFrame.place(x=10, y=80)
+
+yhFrame = tk.Frame(frame, bg=fbgc, width=fw, height=fh)
+yhFrame.place(x=10, y=300)
+
+fbFrame = tk.Frame(frame, bg=fbgc, width=fw, height=fh)
+fbFrame.place(x=10, y=520)
+
+ytImg_open = Image.open('./Asset/yt.png').resize((100,60), Image.ANTIALIAS)
+ytImg = ImageTk.PhotoImage(ytImg_open)
+ytLogo = tk.Label(ytFrame, image=ytImg, borderwidth=0)
+ytLogo.place(x=10, y=20)
+
+# test_open = Image.open('./Asset/google.png').resize((100,100), Image.ANTIALIAS)
+# testImg = ImageTk.PhotoImage(test_open)
+# testLogo = tk.Label(ytFrame, image=testImg, borderwidth=0)
+# testLogo.place(x=150, y=0)
+# testLogo.bind("<Button-1>", lambda x: print(123))
+
+# test_open = Image.open('./Asset/google.png').resize((100,100), Image.ANTIALIAS)
+# testImg = ImageTk.PhotoImage(test_open)
+# testLogo = tk.Button(ytFrame, image=testImg, borderwidth=0)
+# testLogo.place(x=150, y=0)
+# testLogo.bind("<Button-1>", lambda x: print(123))
+
+v1 = Video(ytFrame)
+v1.setUrl('https://i.ytimg.com/vi/b4hTJLtCcUw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCG5gA6AgbGPbjOknIjavjQA5YsLg')
+v1.setPos(130,0)
+v1.setInfo({'title': '【Faker】解謎遊戲天才！芭芭是你系列第一集','author': '中譯版今日韓服精華','views': '觀看次數：113萬次'})
+v1.locate()
+
+v2 = Video(ytFrame)
+v2.setUrl('https://i.ytimg.com/vi/b4hTJLtCcUw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCG5gA6AgbGPbjOknIjavjQA5YsLg')
+v2.setPos(280,0)
+v2.locate()
+
+v3 = Video(ytFrame)
+v3.setUrl('https://i.ytimg.com/vi/b4hTJLtCcUw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCG5gA6AgbGPbjOknIjavjQA5YsLg')
+v3.setPos(430,0)
+v3.locate()
+
+v4 = Video(ytFrame)
+v4.setUrl('https://i.ytimg.com/vi/b4hTJLtCcUw/hqdefault.jpg?sqp=-oaymwEZCNACELwBSFXyq4qpAwsIARUAAIhCGAFwAQ==&rs=AOn4CLCG5gA6AgbGPbjOknIjavjQA5YsLg')
+v4.setPos(580,0)
+v4.locate()
+
+# response = requests.get('https://is2-ssl.mzstatic.com/image/thumb/Purple128/v4/fb/26/01/fb2601b9-27b3-a701-f011-48808eaae98d/source/256x256bb.jpg')
+# img_data = response.content
+# testImg = ImageTk.PhotoImage(Image.open(BytesIO(img_data)))
+# testLogo = tk.Button(ytFrame, image=testImg, borderwidth=0)
+# testLogo.place(x=0, y=0)
+
 
 # populate(frame) ##資料測試
 
---
+++ View/VideoObj.py
@@ -0,0 +1,40 @@
+import tkinter as tk
+import requests
+from PIL import ImageTk, Image
+from io import BytesIO
+
+class Video:
+    
+    def __init__(self, frame):
+        self.w = 140
+        self.h = 200
+        self.fbgc = "#1C1C1C"
+        self.frame = tk.Frame(frame, width=self.w, height=self.h, bg=self.fbgc)
+        self.video = tk.Button(self.frame, borderwidth=0, highlightthickness = 0)
+        self.title = tk.Label(self.frame, wraplength=130, bg=self.fbgc, fg="white", font=('Arial', 10), anchor="n", justify = 'left')
+        self.author = tk.Label(self.frame, bg=self.fbgc, fg='#aaaaaa', font=('Arial', 9))
+        self.views = tk.Label(self.frame, bg=self.fbgc, fg='#aaaaaa', font=('Arial', 9))
+
+    def setUrl(self, url):
+        response = requests.get(url)
+        img_data = response.content
+        img_open = Image.open(BytesIO(img_data)).resize((self.w,100), Image.ANTIALIAS)
+        self.video_img = ImageTk.PhotoImage(img_open)
+        self.video.config(image = self.video_img)
+        
+    def setInfo(self, info):
+        self.title.config(text=info["title"])
+        self.author.config(text=info["author"])
+        self.views.config(text=info['views'])
+
+    def setPos(self, x, y):
+        self.x = x
+        self.y = y
+
+    def locate(self):
+        self.frame.place(x=self.x, y=self.y)
+        self.video.place(x=0, y=10)
+        self.title.place(x=0, y=115, width=self.w, height=35)
+        self.author.place(x=0, y=155, width=self.w)
+        self.views.place(x=0, y=175, width=self.w)
+        
